df65e7c126a242529018ff774ba669a3b167802f
Lobster <polyak.daavid@gmail.com>
2025-10-25 21:24:26 +0200

    glfw setup

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,2 +1,32 @@
-int main() {
+#include <GLFW/glfw3.h>
+
+int main(void) {
+    GLFWwindow* window;
+
+    if (!glfwInit()) return -1;
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+#ifdef __APPLE__
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
+
+    window = glfwCreateWindow(1280, 720, "Renderer", NULL, NULL);
+    if (!window) {
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+
+    while (!glfwWindowShouldClose(window)) {
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
 }
