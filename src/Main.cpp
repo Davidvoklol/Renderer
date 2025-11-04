38c705cf7564f68d91eacace7fd7b62e866f1462
Lobster <polyak.daavid@gmail.com>
2025-11-04 14:33:47 +0100

    Init

---
+++ src/Main.cpp
@@ -0,0 +1,33 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
+
+int main() {
+    GLFWwindow* window;
+
+    if (!glfwInit()) return -1;
+
+    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    if (!window) {
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+		std::cerr << "Failed to initialize OpenGL context\n";
+		return -1;
+	}
+	std::cout << glGetString(GL_VERSION) << "\n";
+
+    while (!glfwWindowShouldClose(window)) {
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
+}
