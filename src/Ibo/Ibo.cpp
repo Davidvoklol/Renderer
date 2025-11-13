18a2c513a0c5b235451530c8809646f62a761fea
Lobster <polyak.daavid@gmail.com>
2025-11-13 15:13:20 +0100

    Ibo(); ~Ibo(); Bind(); GetType()

---
+++ src/Ibo/Ibo.cpp
@@ -0,0 +1,20 @@
+#include "Ibo.hpp"
+
+Ibo::Ibo(unsigned int size, const void* data, unsigned int usage, unsigned int type) {
+	glGenBuffers(1, &id);
+	Bind();
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, size, data, usage);
+	this->type = type;
+}
+
+Ibo::~Ibo() {
+	glDeleteBuffers(1, &id);
+}
+
+void Ibo::Bind() const {
+	Renderer::BindBuffer(GL_ELEMENT_ARRAY_BUFFER, id);
+}
+
+unsigned int Ibo::GetType() const {
+	return type;
+}
