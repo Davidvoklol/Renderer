cbb6139b84cd678794256d47b763a4f5b29cdccc
Lobster <polyak.daavid@gmail.com>
2025-11-15 17:16:05 +0100

    GetCount()

--- src/Ibo/Ibo.hpp
+++ src/Ibo/Ibo.hpp
@@ -2,15 +2,18 @@
 
 #include <glad/glad.h>
 #include "Renderer/Renderer.hpp"
+#include "Common/Common.hpp"
 
 class Ibo {
 	private:
 		unsigned int id;
 		unsigned int type;
+		unsigned int count;
 	public:
 		Ibo(unsigned int size, const void* data, unsigned int usage, unsigned int type);
 		~Ibo();
 
 		void Bind() const;
 		unsigned int GetType() const;
+		unsigned int GetCount() const;
 };
